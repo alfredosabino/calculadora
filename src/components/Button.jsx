bf5efab3f2039854c8dc561d976198f3f4fe7eaf
Alfredo Sabino <alfredo_sabino@hotmail.com>
2021-01-19 21:59:39 -0300

    Create component and style from Display, style class button operations,to event click buttons and grid

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,4 +1,18 @@
 import React from "react";
 import "./Button.css";
 
-export default (props) => <button className="button">{props.label}</button>;
+const Button = (props) => {
+  return (
+    <button
+      onClick={() => props.click(props.label)}
+      className={`button 
+        ${props.operation ? "operation" : ""}
+        ${props.double ? "double" : ""}
+        ${props.triple ? "triple" : ""}`}
+    >
+      {props.label}
+    </button>
+  );
+};
+
+export default Button;
